74fa17c2392c4c3a6eea494e105a27c650d2515b
Shadow <william.shadow@outlook.com>
2022-09-21 01:54:08 +0000

    there

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'WalletBot',
   tagline: 'Tagline here',
-  url: 'https://walletbotteam.github.io',
-  baseUrl: '/docs/',
+  url: 'https://walletbottemp.theshadow.xyz',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'walletbotteam', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'web', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
