606e6e1521e1cc4c938fadcaa8962172a3b4c5c2
Shadow <william.shadow@outlook.com>
2022-11-08 11:16:46 -0600

    fix allcommands

--- src/components/Commands/index.jsx
+++ src/components/Commands/index.jsx
@@ -4,63 +4,77 @@ import axios from "axios"
 import styles from "./index.module.css"
 
 function BotStatus() {
-    const [isLoading, setIsLoading] = useState(true)
-    const [data, setData] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+	const [data, setData] = useState([])
 
-    useEffect(() => {
-        fetch()
-            .then((data) => {
-                setData(data)
-            })
-            .finally(() => {
-                setIsLoading(false)
-            })
-    }, [])
+	useEffect(() => {
+		fetch()
+			.then((data) => {
+				setData(data)
+			})
+			.finally(() => {
+				setIsLoading(false)
+			})
+	}, [])
 
-    const fetch = async () => {
-        try {
-            return (await axios.get(`https://api.walletbot.pro/v0/commands`)).data
-        } catch (_) {
-            return []
-        }
-    }
+	const fetch = async () => {
+		try {
+			return (await axios.get(`https://api.walletbot.pro/v0/commands`))
+				.data
+		} catch (_) {
+			return []
+		}
+	}
 
-    if (isLoading)
-        return (
-            <div className={styles.section}>
-                <h1>Loading ...</h1>
-            </div>
-        )
+	if (isLoading)
+		return (
+			<div className={styles.section}>
+				<h1>Loading ...</h1>
+			</div>
+		)
 
-    return (
-        <section className={styles.section}>
-            <h1>All Commands</h1>
-            <h2>This data is sourced directly from the live bot.</h2>
-            <br />
-            {data.map((x) => {
-                return (
-                    <>
-                        <h3>{x.name}</h3>
-                        <h5>{x.description}</h5>
-                        <p>
-                            Accessible by:{" "}
-                            {x.collabManager
-                                ? "Collab Managers"
-                                : x.walletManager
-                                ? "Wallet Managers"
-                                : x.whitelistManager
-                                ? "Whitelist Managers"
-                                : x.permissions.length > 0
-                                ? "People with the " + x.permissions.join(" and ") + " perms"
-                                : "Anyone"}
-                        </p>
-                        <p>Subcommands: {x.subcommands.length > 0 ? x.subcommands.map(cmd => {{<code>/{x.name} {cmd}</code>}}) : "None"}</p>
-                        <hr />
-                    </>
-                )
-            })}
-        </section>
-    )
+	return (
+		<section className={styles.section}>
+			<h1>All Commands</h1>
+			<h2>This data is sourced directly from the live bot.</h2>
+			<br />
+			{data.map((x) => {
+				return (
+					<>
+						<h3>{x.name}</h3>
+						<h5>{x.description}</h5>
+						<p>
+							Accessible by:{" "}
+							{x.collabManager
+								? "Collab Managers"
+								: x.walletManager
+								? "Wallet Managers"
+								: x.whitelistManager
+								? "Whitelist Managers"
+								: x.permissions.length > 0
+								? "People with the " +
+								  x.permissions.join(" and ") +
+								  " perms"
+								: "Anyone"}
+						</p>
+						<p>
+							Subcommands:{" "}
+							{x.subcommands.length > 0
+								? x.subcommands.map((cmd) => {
+										return (
+											<code>
+												/{x.name} {cmd}
+											</code>
+										)
+								  })
+								: "None"}
+						</p>
+						<hr />
+					</>
+				)
+			})}
+		</section>
+	)
 }
 
 export default BotStatus
